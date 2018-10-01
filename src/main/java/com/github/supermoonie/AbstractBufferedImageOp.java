85dfcb64f4188d1b976d522fbd05ef640a845d1c
supermoonie <wangchao_moonie@163.com>
2018-10-01 10:44:44 +0800

    update

---
+++ src/main/java/com/github/supermoonie/AbstractBufferedImageOp.java
@@ -0,0 +1,45 @@
+package com.github.supermoonie;
+
+import java.awt.*;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
+import java.awt.image.BufferedImage;
+import java.awt.image.BufferedImageOp;
+import java.awt.image.ColorModel;
+
+/**
+ * @author supermoonie
+ * @date 2018/9/29 14:31
+ */
+public abstract class AbstractBufferedImageOp implements BufferedImageOp {
+
+    @Override
+    public Rectangle2D getBounds2D(BufferedImage src) {
+        return new Rectangle(0, 0, src.getWidth(), src.getHeight());
+    }
+
+    @Override
+    public BufferedImage createCompatibleDestImage(BufferedImage src, ColorModel destCM) {
+        if (destCM == null) {
+            destCM = src.getColorModel();
+        }
+        return new BufferedImage(destCM,
+                destCM.createCompatibleWritableRaster(src.getWidth(), src.getHeight()),
+                destCM.isAlphaPremultiplied(),
+                null);
+    }
+
+    @Override
+    public Point2D getPoint2D(Point2D srcPt, Point2D dstPt) {
+        if (dstPt == null) {
+            dstPt = new Point2D.Double();
+        }
+        dstPt.setLocation(srcPt.getX(), srcPt.getY());
+        return dstPt;
+    }
+
+    @Override
+    public RenderingHints getRenderingHints() {
+        return null;
+    }
+}
---
+++ src/main/java/com/github/supermoonie/gui/ImageViewer.java
@@ -0,0 +1,11 @@
+package com.github.supermoonie.gui;
+
+import javax.swing.*;
+
+/**
+ * @author supermoonie
+ * @date 2018/9/30 18:14
+ */
+public class ImageViewer extends JFrame {
+
+}
